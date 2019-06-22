33278c9f2021a04e6809c9f63cdcf3c88ad543ed
sounishnath003 <flock.sinasini@gmail.com>
2019-06-22 15:26:05 +0530

    image isnot comming

--- lib/main.dart
+++ lib/main.dart
@@ -110,47 +110,113 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-Widget _instaList() {
-  return new ListView.builder(
+
+
+
+}
+class _instaList extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+    return new ListView.builder(
     itemCount: 5,
     itemBuilder: (BuildContext context, int index) => index == 0? new SizedBox(
       child: _instaStories(),
+      height: deviceSize.height * 0.2,
     ) : new Column()
     
   );
-}
+  }
 
 Widget _instaStories() {
-  return new Container(
+  return new Container( 
     margin: const EdgeInsets.all(16.0),
     child: new Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        
+        _topText(),
+        _storiesAll(),
       ],
     ),
     );
 }
 
+Widget _topText() {
+return new Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: <Widget>[
+    _storiesButton(),
+    _watchAll(),
+  ],
+);
+}
 
 
+Widget _storiesButton(){
+  return new Text('Stories',
+    style: new TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16.0
+    ),
+    );
+}
 
+Widget _watchAll(){
+  return new Row(
+      children: <Widget>[
+        Icon(Icons.play_arrow),
+        new SizedBox(width: 3.0,),
+        new Text("Watch All",
+        style: new  TextStyle(
+          fontWeight: FontWeight.bold
+        ),
+        )
+      ],
+      );
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+Widget _storiesAll() {
+  return new Expanded(
+    child: new ListView.builder(
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int index) {
+        new Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              width: 100.0,
+              height: 60.0,
+              child: new Text("hhhh"),
+              decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage("https://randomuser.me/api/portraits/men/43.jpg"),
+                )
+              ),
+            ),
+            (index == 0) ? new Positioned(
+              right: 10.0,
+              child: new CircleAvatar(
+                radius: 10.0,
+                backgroundColor: Colors.blueAccent,
+                child: new Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  ),
+              ),
+            ) : new Container()
+          ],
+        );
+      },
+      
+    ),
+  );
+}
 
 
 
