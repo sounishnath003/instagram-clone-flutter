1f31983e2b7535f3a185fae35a7213b6dcccdb59
sounishnath003 <flock.sinasini@gmail.com>
2019-06-22 13:15:34 +0530

    assets and topbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.black,
         iconTheme: IconThemeData(color: Colors.black),
-        primaryTextTheme: TextTheme(title: TextStyle(color: Colors.black)), 
+        primaryTextTheme: TextTheme(title: TextStyle(color: Colors.black, fontFamily: 'Roboto')), 
+        textTheme: TextTheme(title: TextStyle(color: Colors.black)),
       ),
       home: MyHomePage(),
     );
@@ -30,13 +31,39 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-     appBar: new AppBar(
-       title: new Text('Instagram'),
-       centerTitle: true,
-       actions: <Widget>[
-          new Icon(Icons.face)         
-       ],
-     ), 
+     appBar: _topBar(), 
     );
   }
+
+
+Widget _topBar() {
+  return new AppBar(
+    title: new SizedBox(
+      height: 35.0,
+      child: Image.asset("./Assets/ins_logo.png"),
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    leading: new Icon(Icons.camera_alt, color: Colors.black,),
+    elevation: 1.0,
+    actions: <Widget>[
+      new Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: new Icon(Icons.send, color: Colors.black,),
+        )
+    ],
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
 }
